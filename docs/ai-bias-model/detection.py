c9c0053263bcd94c7d53caf912d17fee76d1eb68
Dwin2 <darwin.li@ucc.on.ca>
2023-01-24 08:08:54 -0500

    new ai-bias function

---
+++ docs/ai-bias-model/detection.py
@@ -0,0 +1,86 @@
+totalArticles = 100
+articles, neg_words, pos_words = [], [], []
+names = ["South Asian", "Chinese", "Black", "Filipino", "Arab", "Latin American", "Southeast Asian", "West Asian", "Korean and Japanese", "Indigenous", "White"]
+keywords = {
+    'South Asian' : [[], []],
+    'Chinese' : [[], []],
+    'Black' : [[], []],
+    'Filipino' : [[], []],
+    'Arab' : [[], []],
+    'Latin American' : [[], []],
+    'Southeast Asian' : [[], []],
+    'West Asian' : [[], []],
+    'Korean and Japanese' : [[], []],
+    'Indigenous': [[], []],
+    'White' : [[], []],
+}
+words = {
+    'South Asian' : [[], []],
+    'Chinese' : [[], []],
+    'Black' : [[], []],
+    'Filipino' : [[], []],
+    'Arab' : [[], []],
+    'Latin American' : [[], []],
+    'Southeast Asian' : [[], []],
+    'West Asian' : [[], []],
+    'Korean and Japanese' : [[], []],
+    'Indigenous': [[], []],
+    'White' : [[], []],
+}
+
+for i in range(1, totalArticles+1):
+    file = "./docs/ai-bias-model/articles/article" + str(i) + ".txt"
+    with open(file, "r") as f:
+        articles.append(list(map(str.strip, f.readlines(), )))
+
+with open("./docs/ai-bias-model/adjectives/negative_words.txt", "r") as f:
+    neg_words = list(map(str.strip, f.readlines(), ))
+
+with open("./docs/ai-bias-model/adjectives/positive_words.txt", "r") as f:
+    pos_words = list(map(str.strip, f.readlines(), ))
+
+cnt = 1
+for article in articles:
+    for line in article:
+        for keyword in keywords:
+            if keyword not in line: continue
+            for word in neg_words:
+                if word not in line: continue
+                keywords[keyword][0].append(line)
+                words[keyword][0].append(word)
+            for word in pos_words:
+                if word not in line: continue
+                keywords[keyword][1].append(line)
+                words[keyword][1].append(word)
+    print(cnt)
+    cnt += 1
+
+results = []
+
+for keyword in keywords:
+    print(keyword, len(keywords[keyword][0]), len(keywords[keyword][1]))
+
+for keyword in keywords: results.append([[], []])
+
+for i in range(len(results)):
+    #print(keyword)
+    for line in keywords[names[i]][0]: 
+        if line in results[i][0]: continue
+        results[i][0].append(line)
+    for line in keywords[names[i]][1]: 
+        if line in results[i][1]: continue
+        results[i][1].append(line)
+        #print(line)
+        #print(words[keyword][0])
+        #x = 1
+    
+
+with open("./docs/ai-bias-model/test.txt", "w") as f:
+    for i in range(len(results)):
+        print(len(results[i][0]), len(results[i][1]))
+        f.write(str(results[i][0]))
+        f.write("\n")
+        f.write(str(results[i][1]))
+        f.write("\n")
+        f.write(names[i])
+        f.write("\n")
---
+++ docs/ai-bias-model/web_scraping.py
@@ -0,0 +1,57 @@
+#--------------------- ! DO NOT RUN ! ------------------
+
+import requests, time, sys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+
+sys.exit(0)
+url = 'https://www.cbc.ca/archives'
+
+
+options = webdriver.ChromeOptions()
+# options.add_argument('--ignore-certificate-errors')
+# options.add_argument('--incognito')
+#options.add_argument('--headless')
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=options)
+driver.get(url)
+#more_button = driver.find_element(By.CLASS_NAME, "loadMore")
+time.sleep(100)
+#more_button.click()
+#for i in range(len(more_buttons)):
+  #more_buttons[i].click()
+page_source = driver.page_source
+print(page_source)
+
+#response = requests.get(url)
+
+soup = BeautifulSoup(page_source, 'html.parser')
+#print(soup.title)
+blog_titles = soup.find_all('h3', attrs='headline')
+links = soup.find_all('a', href=True)
+
+
+#for title in blog_titles:
+#    print(title.text)
+invalid = ["https://www.cbc.ca/archives", "https://cbc.ca/archives/arts", "https://cbc.ca/archives/history", "https://cbc.ca/archives/sports"]
+count = 0
+for i in links:
+    if "https:" not in i['href']: link = "https://cbc.ca" + i['href']
+    if "/news/" in link or ('/archives/' in link and link not in invalid):
+        try:
+            count += 1
+            print(link)
+            response2 = requests.get(link)
+            soup2 = BeautifulSoup(response2.text, 'html.parser')
+            texts = soup2.find_all('p')
+            title = soup2.find_all('h1', attrs='detailHeadline')[0].text
+            file_name = "./src/model/articles/article" + str(count) + ".txt"
+            with open(file_name, "w") as f:
+                f.write(title + "\n\n")
+                for line in texts:
+                    f.write(line.text)
+                    f.write("\n")
+        except requests.exceptions.ConnectionError: 
+            print("Failed")
+            continue
+
